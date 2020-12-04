4ac7f6033f56375f70ca3bf0f3cc16c1fda5849f
michael garrison <mgarrison.sa@gmail.com>
2020-12-03 19:36:03 -0600

    working with numbers

---
+++ PythonCodeCamp/Numbers.py
@@ -0,0 +1,29 @@
+from math import *
+
+print(7)
+print(-7)
+
+print(3 + 4.5)
+print(5 - 4)
+print(3 * 4)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+
+print(10 % 3)
+
+my_num = 34
+print(my_num)
+print(str(my_num))
+print(str(my_num) + " my favorite number")
+
+neg_num = -5
+print(abs(neg_num))
+
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.7))
+
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
