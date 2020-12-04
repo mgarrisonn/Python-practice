e33f21f8c8e994aa24c217f598fa5f0da17e4304
michael garrison <mgarrison.sa@gmail.com>
2020-12-03 19:35:47 -0600

    working with strings

---
+++ PythonCodeCamp/Strings.py
@@ -0,0 +1,21 @@
+print("Printing String")
+print("Printing\nString")
+print("Printing\"String")
+print("Printng\String")
+
+phrase = "Printing string"
+print(phrase)
+print(phrase + " with python")
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+print(len(phrase))
+
+print(phrase[0])
+print(phrase.index("P"))
+
+print(phrase.replace("Printing", "Replacing"))
