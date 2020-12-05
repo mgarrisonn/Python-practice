60140e5ade2e9c4bfc87c817a98515d47b8c5f16
michael garrison <mgarrison.sa@gmail.com>
2020-12-04 22:24:34 -0600

    intro to lists/tuples

---
+++ PythonCodeCamp/Lists.py
@@ -0,0 +1,10 @@
+friends = ["Kevin", "Jason", "Trey", "Alan", "Joe"]
+friends[1] = "Michael"
+
+print(friends)
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:4])
---
+++ PythonCodeCamp/Tuples.py
@@ -0,0 +1,4 @@
+coordinates = [(4, 5), (6, 7), (80, 34)]
+print(coordinates[0])
+print(coordinates[1])
+
