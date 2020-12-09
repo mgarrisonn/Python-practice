0913911ce536e75a66ba20d1c7c09e09cbd50653
michael garrison <mgarrison.sa@gmail.com>
2020-12-08 21:23:32 -0600

    intro to for loop

---
+++ PythonCodeCamp/ForLoop.py
@@ -0,0 +1,27 @@
+
+for letter in "Learning Python":
+    print(letter)
+
+friends =["Jim", "Jane", "Kevin"]
+for name in friends:
+    print(name)
+
+for index in range(10):
+    print(index)
+
+for index in range(3, 10):
+    print(index)
+
+len(friends)
+for index in range(len(friends)):
+    print(friends[index])
+
+len(friends)
+for index in range(len(friends)):
+    print(index)
+
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("Not first")
