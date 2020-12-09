fdaac9abdeadc01ece6924cd22fd22f533beb2d5
michael garrison <mgarrison.sa@gmail.com>
2020-12-08 21:22:51 -0600

    intro to dictionaries

---
+++ PythonCodeCamp/Dictionaries.py
@@ -0,0 +1,18 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "Febuary",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Dec"))
+
