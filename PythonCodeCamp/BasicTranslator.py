ffddd17aad97faf498a1732fc4b4b7e7cd0695ad
michael garrison <mgarrison.sa@gmail.com>
2020-12-09 22:30:20 -0600

    python basic translator

---
+++ PythonCodeCamp/BasicTranslator.py
@@ -0,0 +1,20 @@
+
+# My Language
+# vowel -> g
+# -----------
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
+
