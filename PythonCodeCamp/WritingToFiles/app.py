32943608fbfd4bfe70db1b630e6b35092440c78f
michael garrison <mgarrison.sa@gmail.com>
2020-12-10 20:52:35 -0600

    intro to writing to files with python

---
+++ PythonCodeCamp/WritingToFiles/app.py
@@ -0,0 +1,9 @@
+
+# employee_file = open("employees1.txt", "w")
+employee_file = open("index.html", "w")
+
+# employee_file.write("Toby - Human Resources")
+# employee_file.write("\nKelly - Customer Service")
+employee_file.write("<p>This is HTML<p>")
+
+employee_file.close()
