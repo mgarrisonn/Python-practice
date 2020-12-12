5f8ced325b2c3a2f64d5e9d58a3c12f0f92e90cc
michael garrison <mgarrison.sa@gmail.com>
2020-12-11 21:59:52 -0600

    intro to object functions

---
+++ PythonCodeCamp/ObjectFunctions/app.py
@@ -0,0 +1,8 @@
+from Students import Students
+
+student1 = Students("Oscar", "Accounting", 3.1)
+student2 = Students("Phyllis", "Business", 3.8)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
+
