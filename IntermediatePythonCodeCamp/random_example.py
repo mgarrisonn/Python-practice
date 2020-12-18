d841b7393ca0b95debe14d180b7ebe4af7710b11
michael garrison <mgarrison.sa@gmail.com>
2020-12-17 20:16:28 -0600

    working with random numbers

--- IntermediatePythonCodeCamp/random_example.py
+++ IntermediatePythonCodeCamp/random_example.py
@@ -1,4 +1,6 @@
 import random
+import secrets
+import numpy as np
 
 a = random.random()
 print(a)
@@ -50,5 +52,38 @@ print(random.randint(1, 10))
 random.seed(2)
 print(random.random())
 print(random.randint(1, 10))
+
+
+a = secrets.randbelow(10)
+print(a)
+
+a = secrets.randbits(4)
+print(a)
+
+mylist = list("ABCDEFGH")
+a = secrets.choice(mylist)
+print(a)
+
+a = np.random.rand(3)
+print(a)
+
+a = np.random.rand(3, 3)
+print(a)
+
+a = np.random.randint(0, 10, 3)
+print(a)
+
+a = np.random.randint(0, 10, (3, 4))
+print(a)
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr)
+np.random.shuffle(arr)
+print(arr)
+
+np.random.seed(1)
+print(np.random.rand(3, 3))
+np.random.seed(1)
+print(np.random.rand(3, 3))
 
 
