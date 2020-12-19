9f28084a66a0babbdf416333e31dd65d51202ff4
michael garrison <mgarrison.sa@gmail.com>
2020-12-18 19:07:25 -0600

    working with generators

--- IntermediatePythonCodeCamp/generators.py
+++ IntermediatePythonCodeCamp/generators.py
@@ -75,3 +75,25 @@ print(sys.getsizeof(firstn(1000000)))
 print(sys.getsizeof(firstn_generator(1000000)))
 
 
+def fibonacci(limit):
+    a, b = 0, 1
+    while a < limit:
+        yield a
+        a, b = b, a + b
+
+
+fib = fibonacci(30)
+for i in fib:
+    print(i)
+
+
+mygenerator = (i for i in range(10) if i % 2 == 0)
+for i in mygenerator:
+    print(i)
+
+print(list(mygenerator))
+print(sys.getsizeof(mygenerator))
+
+mylist = [i for i in range(10) if i % 2 == 0]
+print(mylist)
+print(sys.getsizeof(mylist))
